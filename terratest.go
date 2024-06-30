bc095e8b3db6f6b186fb3f4a6dbc54601efb18ee
sergei.strukov <strukov.sergei.999@gmail.com>
2024-06-30 13:01:17 +0300

    changed pipeline

---
+++ terratest.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+    "fmt"
+    "testing"
+
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestTerraformEks(t *testing.T) {
+    t.Parallel()
+
+    terraformOptions := &terraform.Options{
+        TerraformDir: "../",
+        Vars: map[string]interface{}{
+            // Add any variables you want to override here
+        },
+    }
+
+    defer terraform.Destroy(t, terraformOptions)
+
+    // Run terraform init and terraform apply. Fail the test if there are any errors.
+    terraform.InitAndApply(t, terraformOptions)
+
+    // Run terraform output to get the value of an output variable
+    clusterName := terraform.Output(t, terraformOptions, "cluster_name")
+
+    // Verify that the cluster name is as expected
+    assert.NotEmpty(t, clusterName)
+    fmt.Printf("Cluster name is: %s\n", clusterName)
+}
